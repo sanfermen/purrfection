e550c501a9ac4f56d6d86cf3b08a8035ec4aac52
Cecilia <ceci_2504@hotmail.com>
2025-04-08 16:43:40 +0200

    modelo appointment

---
+++ src/models/appointment.js
@@ -0,0 +1,29 @@
+import { DataTypes, STRING } from "sequelize";
+import connection from "../config/sequelize.js";
+
+//modelo de la tabla Appointment
+const Appointment = connection.define("appointment", {
+    appointment_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    start_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    end_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING(255),
+    },
+    user_id:{
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    }
+})
+
+export default Appointment;
