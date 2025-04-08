7271421fdff564c58c3bdc7f5f97fc81e1097c8c
Cecilia <ceci_2504@hotmail.com>
2025-04-08 16:32:53 +0200

    modelo Cat

---
+++ src/models/cat.js
@@ -0,0 +1,45 @@
+import { DataTypes, STRING } from "sequelize";
+import connection from "../config/sequelize.js";
+
+//modelo de la tabla Cat
+const Cat = connection.define("cat", {
+    cat_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+    },
+    age: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+    },
+    neuter: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+    comments: {
+        type: DataTypes.STRING(255)
+    },
+    special_needs: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+    breed: {
+        type: DataTypes.STRING(45),
+    },
+    image: {
+        type: DataTypes.STRING(300),
+    },
+    user_id:{
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    }
+})
+
+export default Cat;
